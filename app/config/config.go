f800507ffd115e8d048c3ed3c85f1cb6a67738fb
meesooqa <meesooqa@hotmail.com>
2025-02-19 09:28:31 +0300

    config

---
+++ app/config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+// Conf from config yml
+type Conf struct {
+	Mongo *MongoConfig `yaml:"mongo"`
+}
+
+// MongoDB parameters
+type MongoConfig struct {
+	URI                string `yaml:"uri"`
+	Database           string `yaml:"database"`
+	CollectionMessages string `yaml:"collection_messages"`
+}
+
+// Load config from file
+func Load(fname string) (res *Conf, err error) {
+	res = &Conf{}
+	data, err := os.ReadFile(fname)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := yaml.Unmarshal(data, res); err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
---
+++ app/config/config_test.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLoad(t *testing.T) {
+	c, err := Load("testdata/config.yml")
+	require.NoError(t, err)
+
+	assert.Equal(t, "mongodb://localhost:27017", c.Mongo.URI)
+	assert.Equal(t, "database_name", c.Mongo.Database)
+	assert.Equal(t, "messages_collection_name", c.Mongo.CollectionMessages)
+}
+
+func TestLoadConfigNotFoundFile(t *testing.T) {
+	r, err := Load("/tmp/1b154315-b263-4952-b2b8-fad031f0df4f.txt")
+	assert.Nil(t, r)
+	assert.EqualError(t, err, "open /tmp/1b154315-b263-4952-b2b8-fad031f0df4f.txt: no such file or directory")
+}
+
+func TestLoadConfigInvalidYaml(t *testing.T) {
+	r, err := Load("testdata/file.txt")
+
+	assert.Nil(t, r)
+	assert.EqualError(t, err, "yaml: unmarshal errors:\n  line 1: cannot unmarshal !!str `Not Yaml` into config.Conf")
+}
