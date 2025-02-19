0a198249e3b3356062a7cf134e873b95274003fe
meesooqa <meesooqa@hotmail.com>
2025-02-19 13:36:27 +0300

    rollback of separating data saving

--- app/main.go
+++ app/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"os"
 	"sync"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 
 	"github.com/meesooqa/ttag/app/config"
 	"github.com/meesooqa/ttag/app/db"
@@ -12,6 +14,8 @@ import (
 	"github.com/meesooqa/ttag/app/repositories"
 	"github.com/meesooqa/ttag/app/tg"
 )
+
+var logger *zap.Logger
 
 func main() {
 	var wg sync.WaitGroup
@@ -42,4 +46,25 @@ func main() {
 
 	wg.Wait()
 	logger.Info("all goroutines are done")
+}
+
+func initLogger() {
+	logFile, _ := os.OpenFile("var/log/app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	writer := zapcore.AddSync(logFile)
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:      "timestamp",
+		LevelKey:     "level",
+		MessageKey:   "message",
+		EncodeTime:   zapcore.ISO8601TimeEncoder,
+		EncodeLevel:  zapcore.CapitalLevelEncoder,
+		EncodeCaller: zapcore.ShortCallerEncoder,
+	}
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(encoderConfig),
+		writer,
+		zap.DebugLevel, // zap.InfoLevel
+	)
+
+	logger = zap.New(core)
+	defer logger.Sync()
 }
